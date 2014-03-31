bfaa139486ea5835a169dd31e15ef3e485566326
Sam D (TheComputerGeek2) <samjdixon@comcast.net>
2014-03-31 11:13:21 -0700

    Added javadocs to the input field class

--- Pi-Day-Project/src/gui/PiInputField.java
+++ Pi-Day-Project/src/gui/PiInputField.java
@@ -13,8 +13,14 @@ import main.Main;
 
 public class PiInputField extends JTextField implements KeyListener {
 
+	/**
+	 * Represents the file that contains the digits of pi. This should be fixed later.
+	 */
 	public static final String PI_FILE = "W:\\Staff_Student\\jjmiller\\Writeable\\Pi.txt";
 
+	/**
+	 * Instantiates the default input field for the gui. Loads with the listeners ready and the font set.
+	 */
 	public PiInputField() {
 		super();
 		new AccuracyChecker(this, new File(PiInputField.PI_FILE));
@@ -47,12 +53,20 @@ public class PiInputField extends JTextField implements KeyListener {
 	public void keyTyped(KeyEvent arg0) {
 
 	}
-
+	
+	/**
+	 * Prevents the user from making further edits to the input field and changes the background to red.
+	 * Should be called when the user makes a mistake.
+	 */
 	public void failSequence() {
 		this.setEditable(false);
 		this.setBackground(Color.RED);
 	}
 
+	/**
+	 * Resets the input field and background so that it can be edited again and
+	 * the background color is back in the state indicating that it the user has not yet failed.
+	 */
 	public void reset() {
 		this.setBackground(Color.YELLOW);
 		this.setEditable(true);
